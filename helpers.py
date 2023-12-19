12424114750cb7cf20eaeb2a820bc757c9f3b797
Jakhongir Saydaliev <68238593+Jakhongir0103@users.noreply.github.com>
2023-12-19 18:26:36 +0100

    sentiment analysis and 3.2 is left

--- helpers.py
+++ helpers.py
@@ -13,6 +13,219 @@ from gensim import corpora, models
 # nltk.download('stopwords')
 # nltk.download('wordnet')
 
+def get_correlations(df_vd_tech):
+    '''
+    df_vd_tech: dataframe to use for computing the correlations
+    
+    returns: pearson correlation between duration and # of likes as well as duration and # of dislikes
+    '''
+    # filter only the tech review videos by tech review channels
+    df_vd_tech["upload_date"] = pd.to_datetime(df_vd_tech["upload_date"])
+    df_vd_tech["upload_year"] = df_vd_tech['upload_date'].dt.year
+
+    # correlation between the duration of the video and number of likes and dislikes
+    df_vd_tech_filtered = df_vd_tech.copy()
+    # just to avoid division by 0
+    df_vd_tech_filtered["view_count"] = df_vd_tech_filtered["view_count"].replace({0:1})
+    #correlation between like count and duration
+    df_vd_tech_filtered["like_count_ratio"] = df_vd_tech_filtered['like_count']/df_vd_tech_filtered["view_count"]
+    df_vd_tech_filtered["dislike_count_ratio"] = df_vd_tech_filtered['dislike_count']/df_vd_tech_filtered["view_count"]
+    pearson_like_duration_corrs = []
+    pearson_dislike_duration_corrs = []
+
+    years = np.unique(df_vd_tech_filtered["upload_year"])
+    for year in years:
+        #compute pearson correlation
+        df = df_vd_tech_filtered[df_vd_tech_filtered["upload_year"]==year].dropna()
+        corr_like = stats.pearsonr(df["duration"], df['like_count_ratio'])
+        corr_dislike = stats.pearsonr(df["duration"], df['dislike_count_ratio'])
+        pearson_like_duration_corrs.append(corr_like)
+        pearson_dislike_duration_corrs.append(corr_dislike)
+
+    df_corrs_pearson_like = pd.DataFrame(pearson_like_duration_corrs, columns=["pearson_corr", "p_value"], index=years)
+    df_corrs_pearson_dislike = pd.DataFrame(pearson_dislike_duration_corrs, columns=["pearson_corr", "p_value"], index=years) 
+    
+    return df_corrs_pearson_like, df_corrs_pearson_dislike
+
+def plot_correlations(df_corrs_pearson_like, df_corrs_pearson_dislike):
+    '''
+    df_corrs_pearson_like: dataframe containing pearson correltions with their p-values between likes and duration per year
+    df_corrs_pearson_dislike: dataframe containing pearson correltions with their p-values between dislikes and duration per year
+
+    plots the pearson correlations as a bar plot
+    '''
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
+    colors = sns.color_palette("colorblind",2)
+
+    # Create a bar plot for Pearson correlation coefficients
+    bar_like = axs[0].bar(df_corrs_pearson_like.index, df_corrs_pearson_like["pearson_corr"], color=colors[0], alpha=0.7)
+    bar_dislike = axs[1].bar(df_corrs_pearson_dislike.index, df_corrs_pearson_dislike["pearson_corr"], color=colors[0], alpha=0.7)
+
+    # Highlight bars where p-values are less than 0.05
+    for bar, p_value in zip(bar_like, df_corrs_pearson_like["p_value"]):
+        if p_value < 0.05:
+            bar.set_color(colors[1])
+    for bar, p_value in zip(bar_dislike, df_corrs_pearson_dislike["p_value"]):
+        if p_value < 0.05:
+            bar.set_color(colors[1])
+
+    # Set labels and title
+    axs[0].set_title('# Likes vs Duration')
+    axs[1].set_title('# Dislikes vs Duration')
+
+    # Display a legend indicating significance
+    plt.legend(handles=[plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=colors[1], markersize=10, label='p < 0.05')],
+               loc='upper right')
+    plt.suptitle('Pearson Correlation Coefficients per Year')
+    plt.tight_layout()
+    plt.show()
+
+def compute_ratios_and_print_duration(df_vd_tech):
+    '''
+    computes the like to dislike ratio and prints the duration
+    
+    df_vd_tech: dataframe to use for the statistics computation
+    
+    retuns the dataframe containing the ratio
+    '''
+    
+    # get the likes to dislikes ratio
+    df_vd_tech_ratio = df_vd_tech[['channel_id', 'upload_year','like_count','dislike_count','display_id','duration']]
+    df_vd_tech_ratio['dislike_count'] = df_vd_tech_ratio['dislike_count'].replace({0:1})
+    df_vd_tech_ratio['dislike_count'].fillna(1,inplace=True)
+    df_vd_tech_ratio['like_dislike_ratio'] = df_vd_tech_ratio['like_count'] / df_vd_tech_ratio['dislike_count']
+    df_vd_tech_ratio.like_dislike_ratio.fillna(0,inplace=True)
+
+    #group videos per channel and compute the average duration and like to dislike ratio (per channel). 
+    #Sort by the latter and get the top 100 and bottom 100 like to dislike ratios and compute the average of 
+    #their duration averages (macro average). Note that by computing average per channel we give every channel the same weight.
+    df_vd_tech_sorted = df_vd_tech_ratio.groupby("channel_id")[["duration", "like_dislike_ratio"]].mean().sort_values(by='like_dislike_ratio', ascending=False)
+    print("The average duration of a video from the top 10% of channels (in terms of like to dislike ratio) is {:.0f} seconds and the average duration of a video from the bottom 10% of channels is {:.0f}.".format(\
+    df_vd_tech_ratio.iloc[0:(int)(0.1*len(df_vd_tech_ratio))].duration.mean(), df_vd_tech_ratio.iloc[(int)(0.9*len(df_vd_tech_ratio)):].duration.mean()))
+    print(stats.ttest_ind(df_vd_tech_ratio.iloc[0:(int)(0.1*len(df_vd_tech_ratio))].duration, df_vd_tech_ratio.iloc[(int)(0.9*len(df_vd_tech_ratio)):].duration))
+
+    return df_vd_tech_ratio
+
+def plot_helper(df_vd_tech):
+    '''
+    helper function to plot various statistics
+    
+    df_vd_tech: dataframe to use for the statistics computation
+    '''
+    # just to avoid division by 0
+    df_vd_tech['dislike_count'] = df_vd_tech['dislike_count'].replace({0:1})
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 6), sharex=True)
+    colors = sns.color_palette("colorblind",1)
+
+    sns.pointplot(x=df_vd_tech['upload_year'],
+                  y=df_vd_tech['like_count'],
+                  errwidth=1.5,
+                  color=colors[0],
+                  ax=axs[0][0])
+
+    sns.pointplot(x=df_vd_tech['upload_year'],
+                  y=df_vd_tech['dislike_count'],
+                  errwidth=1.5,
+                  color=colors[0],
+                  ax=axs[0][1])
+    
+    sns.pointplot(x=df_vd_tech['upload_year'],
+                  y=df_vd_tech['like_count'] / df_vd_tech['dislike_count'],
+                  errwidth=1.5,
+                  color=colors[0],
+                  ax=axs[1][0])
+
+    sns.pointplot(x=df_vd_tech['upload_year'],
+                  y=df_vd_tech['duration'],
+                  errwidth=1.5,
+                  color=colors[0],
+                  ax=axs[1][1])
+
+    axs[0][0].set(title='Average number of likes per year', xlabel=None)
+    axs[0][0].grid(axis='y', linestyle='--')
+    axs[0][0].tick_params(axis='x', rotation=45)
+    axs[0][0].set_xlabel('years')
+    
+    axs[0][1].set(title='Average number of dislikes per year', xlabel=None)
+    axs[0][1].grid(axis='y', linestyle='--')
+    axs[0][1].tick_params(axis='x', rotation=45)
+    axs[0][1].set_xlabel('years')
+    
+    axs[1][0].set(title='Average ratio of like to dislike per video per year', xlabel=None)
+    axs[1][0].grid(axis='y', linestyle='--')
+    axs[1][0].tick_params(axis='x', rotation=45)
+    axs[1][0].set_xlabel('years')
+    axs[1][0].set_ylabel("like-dislike ratio")
+    
+    axs[1][1].set(title='Average video duration per year', xlabel=None)
+    axs[1][1].grid(axis='y', linestyle='--')
+    axs[1][1].tick_params(axis='x', rotation=45)
+    axs[1][1].set_xlabel('years')
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_moving_avg(df_vd_tech_ratio, window, short):
+    '''
+    df_vd_tech_ratio: dataframe to be used to compute the moving avg
+    window: window of the moving average
+    short: either False or True for either long or short videos respectively
+    '''
+    # define color
+    colors = sns.color_palette("colorblind",1)
+
+    ## sort videos by duration
+    df_vd_tech_ratio.sort_values(by='duration',inplace=True)
+
+    # plot moving average of likes to dislikes ratio versus duration
+    plt.figure(figsize=(6,3))
+    y = df_vd_tech_ratio["like_dislike_ratio"].rolling(window).mean().values
+    x = df_vd_tech_ratio['duration']
+    #if short videos only plot from 0 to 20 minutes else from 20 minutes to the longest duration
+    if(short):
+        plt.xlim(0,max(df_vd_tech_ratio['duration']))
+    else:
+        plt.xlim(1200, max(df_vd_tech_ratio['duration']))
+        plt.xscale('log')
+        plt.xticks([1200, 10000, 100000])
+    ax = sns.lineplot(x=x, y=y, color=colors[0])
+    ax.set(title='Moving average of likes-to-dislikes ratio over the duration of video', xlabel='Duration (seconds)', ylabel='#likes / #dislikes')
+    plt.show()
+
+def plot_statistics_across_duration_intervals(df_vd_tech_ratio, threshold):
+    '''
+    df_vd_tech_ratio: dataframe to use to compute the statistics
+    threshold: int taking 2 possible values: if 2004 take all videos and if 2010 take videos from 2011 to 2019
+    '''
+    df_vd_tech_ratio['duration_intervals'] = pd.cut(df_vd_tech_ratio['duration'], bins=10)
+
+    fig, axs = plt.subplots(nrows=4, ncols=1, sharex=True, figsize=(12, 12))
+    colors = sns.color_palette("colorblind",4)
+    fig.tight_layout()
+    fig.suptitle("mean of like_dislike_ratio, like_count, dislike_count and # of videos for each duration interval for videos uploaded after 2010", y=1)
+
+    number_of_videos_per_interval = df_vd_tech_ratio[df_vd_tech_ratio['upload_year']>threshold].groupby('duration_intervals', as_index=False).apply(lambda x: len(x)).rename(columns={None: 'number_of_videos'})
+    sns.barplot(x='duration_intervals', y='like_dislike_ratio', data=df_vd_tech_ratio[df_vd_tech_ratio['upload_year']>threshold], ax=axs[0], estimator ='mean', color=colors[0])
+    sns.barplot(x='duration_intervals', y='like_count', data=df_vd_tech_ratio[df_vd_tech_ratio['upload_year']>threshold], ax=axs[1], color=colors[1])
+    sns.barplot(x='duration_intervals', y='dislike_count', data=df_vd_tech_ratio[df_vd_tech_ratio['upload_year']>threshold], ax=axs[2], color=colors[2])
+    sns.barplot(x='duration_intervals', y='number_of_videos', data=number_of_videos_per_interval, ax=axs[3], color=colors[3])
+
+    #plot the like and dislike count on the same log scale
+    axs[0].set(xlabel=None)
+
+    axs[1].set(xlabel=None)
+    axs[1].set_yscale('log')
+
+    axs[2].set(xlabel=None)
+    axs[2].set_yscale('log')
+
+    axs[3].set(xlabel="Duration intervals")
+    axs[3].tick_params(axis='x', rotation=20)
+
+    plt.tight_layout()
+    plt.show()
+
 def find_in_name_and_tags(df: pd.DataFrame, item: str, release_date: pd.Timestamp) -> pd.DataFrame:
     """
     Filter a DataFrame based on a specified product in the title or tags column,
