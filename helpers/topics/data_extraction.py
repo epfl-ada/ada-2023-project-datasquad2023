e4b10a5e8dea87ac3754b2dbcebf558ff3f68560
Jakhongir Saydaliev <68238593+Jakhongir0103@users.noreply.github.com>
2023-12-22 10:26:09 +0100

    almost done

---
+++ helpers/topics/data_extraction.py
@@ -0,0 +1,144 @@
+import datetime
+import pandas as pd
+from tqdm import tqdm
+
+import json
+import os
+import re
+
+class DataExtractor():
+    def __init__(self, config, video_data) -> None:
+        self.config = config
+        self.video_data = video_data.copy()
+        self.channel_data = pd.read_csv(config['channel_data_path'], sep='\t', compression='infer')
+
+    def process_df(self, df):
+        df['upload_date'] = pd.to_datetime(df['upload_date'])
+        return df
+    
+    def products_data(self, products: list, releases: list) -> dict:
+        """
+        Returns a dictionary of dataframes for each product
+        Where each dataframe only contains videos about that product
+        :param products: list of product names
+        :param releases: list of release dates
+        :return: dictionary of dataframes
+        """
+        df = self.video_data.copy()
+        products_df = {p: [] for p in products}
+        for product_name, release_date in zip(products, releases):
+            # change type of title to str
+            df['title'] = df['title'].astype(str)
+            item_df = df[df['title'].str.contains(product_name, case=False)]
+
+            item_df["upload_date"] = pd.to_datetime(df["upload_date"])
+            item_df = item_df[(item_df['upload_date'] >= datetime.datetime.strptime(release_date, '%d-%m-%Y') - datetime.timedelta(days=180)) & (item_df['upload_date'] <= datetime.datetime.strptime(release_date, '%d-%m-%Y') + datetime.timedelta(days=180))]
+
+            if len(item_df) > 0:
+                products_df[product_name].append(item_df)
+            
+        for product_name in products:
+            products_df[product_name] = pd.concat(products_df[product_name])
+
+        return products_df
+    
+    def ratio_views(self, product_df):
+        """
+        Returns a dictionary of the ratio of views of each product
+        to the total views of all videos uploaded on that day
+        :param product_df: dictionary of dataframes
+        :return: dictionary of ratios
+        """
+        youtube_views = self.video_data[['upload_date', 'view_count']].groupby('upload_date').sum().reset_index()
+        youtube_views = youtube_views.rename(columns={'view_count': 'youtube_views'})
+        product_views = product_df[['upload_date', 'view_count']].groupby('upload_date').sum().reset_index()
+        product_views = product_views.rename(columns={'view_count': 'product_views'})
+
+        # convert upload_date to string
+        youtube_views['upload_date'] = youtube_views['upload_date'].astype(str)
+        product_views['upload_date'] = product_views['upload_date'].astype(str)
+
+        product_views = product_views.merge(youtube_views, on='upload_date')
+        product_views = product_views.dropna()
+
+        # compute the ratio of product views to youtube views
+        product_views['view_ratio'] = product_views['product_views']/product_views['youtube_views']
+
+        # now reconvert upload_date to datetime
+        product_views['upload_date'] = pd.to_datetime(product_views['upload_date'])
+
+        product_views = product_views.set_index('upload_date').to_dict()['view_ratio']
+        return product_views
+    
+    def get_topic_info(self, data: pd.DataFrame, release: str, start: str, end: str, topic: list = None) -> dict:
+        """
+        Returns a dictionary of the average views and subs of videos
+        about a specific topic from start to end
+        :param df: dataframe of videos
+        :param topic: list of keywords
+        :param start: start date
+        :param end: end date
+        :return: dictionary of average views and subs
+        """
+        df = data.copy()
+        # convert datetime to datetime object
+        df['upload_date'] = pd.to_datetime(df['upload_date'])
+        if topic:
+            df = df[
+                (df['title'].str.contains('|'.join(topic), case=False)) & 
+                (df['upload_date'] > datetime.datetime.strptime(start, '%Y-%m-%d')) & 
+                (df['upload_date'] < datetime.datetime.strptime(release, '%Y-%m-%d'))
+            ]
+        df = df[(df['upload_date'] > datetime.datetime.strptime(start, '%Y-%m-%d')) & (df['upload_date'] < datetime.datetime.strptime(end, '%Y-%m-%d'))]
+
+        # convert start and end to dates
+        start = datetime.datetime.strptime(start, '%Y-%m-%d')
+        end = datetime.datetime.strptime(end, '%Y-%m-%d')
+
+        product_channels = df['channel_id'].unique()
+        channels_df = self.channel_data.copy()
+
+        # keep only channels that published pre-release videos
+        channels_df = channels_df[channels_df['channel'].isin(product_channels)]
+        # convert datetime to datetime object and date format
+        channels_df['datetime'] = pd.to_datetime(channels_df['datetime'])
+        # keep only rows that have datetime between 180 days before the release date and the release date
+        channels_df = channels_df[(channels_df['datetime'] > start) & (channels_df['datetime'] < end)]
+        channels_df['datetime'] = channels_df['datetime'].dt.date
+        channels_df = channels_df.groupby('datetime').sum().reset_index()
+        channels_df.sort_values('datetime',ascending=True,inplace=True)
+
+        channels_df['ratio_views'] = channels_df['delta_views'] / channels_df['views']
+        channels_df['ratio_subs'] = channels_df['delta_subs'] / channels_df['subs']
+
+        # make a moving average of the ratio of views and subs
+        channels_df['ratio_views'] = channels_df['ratio_views'].rolling(window=5).mean()
+        channels_df['ratio_subs'] = channels_df['ratio_subs'].rolling(window=5).mean()
+
+        channels_df['datetime'] = pd.to_datetime(channels_df['datetime']).dt.date
+        channels_df = channels_df[['datetime', 'ratio_views', 'ratio_subs']]
+        # turn into dictionary
+        topic_info = channels_df.set_index('datetime').to_dict()
+
+        return topic_info    
+    def get_channels_product(self, product_name, release_date):
+        """
+        Given a dataframe with columns: channel_id, title and upload_date, get
+        the channels that did not upload any video about the product before the
+        release date of the product
+        :param product_name: name of product
+        :param release_date: release date of product
+        :return: list of channel ids
+        """
+        df = self.video_data.copy()
+        df = df[['channel_id', 'title', 'upload_date']]
+        # df['title'] = df['title'].astype(str)
+        df = df[df['title'].str.contains(product_name, case=False)]
+        df['upload_date'] = pd.to_datetime(df['upload_date'])
+        df = df[df['upload_date'] < datetime.datetime.strptime(release_date, '%d-%m-%Y')]
+        df = df.drop_duplicates(subset=['channel_id'])
+
+        # filter from video_data the channl_ids in df
+        channels = self.video_data[~self.video_data['channel_id'].isin(df['channel_id'])]
+
+        return channels
---
+++ helpers/topics/data_vis.py
@@ -0,0 +1,111 @@
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
+import datetime as dt
+import plotly.graph_objects as go
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_product_hist(x, y, release_date, title, ax):
+    # convert x from list of string to list of datetime
+    # x = [dt.datetime.strptime(date, '%Y-%m-%d') for date in x]
+    start_date = x[0]
+
+    # Define the specific time ranges as 1 month before and after the release date
+    time_before_specific = dt.datetime.strptime(release_date, '%d-%m-%Y') - dt.timedelta(days=30)
+    time_after_specific = dt.datetime.strptime(release_date, '%d-%m-%Y') + dt.timedelta(days=30)
+
+    # Convert dates to days
+    days = np.array([(date - start_date).days for date in x])
+
+    # Categorize the days into three groups based on time
+    colors = np.zeros(len(x), dtype=int)
+    colors[days < (time_before_specific - start_date).days] = 0  # before
+    colors[(days >= (time_before_specific - start_date).days) & 
+        (days < (time_after_specific - start_date).days)] = 1  # during
+    colors[days >= (time_after_specific - start_date).days] = 2  # after
+
+    # Set a color palette for the plot
+    color_palette = ['firebrick', 'gold', 'deepskyblue']
+
+    # Plot the histogram with colored bars
+    for col in np.unique(colors):
+        # add a bar plot for each color
+        ax.bar(x=np.array(x)[colors == col], height=np.array(y)[colors == col], color=color_palette[col], edgecolor='black', alpha=0.7, label=f'Color {col}', width=1)
+
+
+    # add a grey vertical line for the release date
+    ax.axvline(dt.datetime.strptime(release_date, '%d-%m-%Y'), color='red', linewidth=1, linestyle='--', label=f'Release date: {release_date}')
+
+    # Customize the plot
+    ax.set_title(title)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Frequency')
+    ax.tick_params(axis="x",rotation=20)
+
+
+    # add legend for colors
+    legend_elements = [ax.bar(x=[0], height=0, width=0, color=color_palette[col], alpha=0.7, label=f'Color {col}')[0] for col in np.unique(colors)]
+    legend_elements.append(ax.axhline(0, color='black', linewidth=0.5))
+    ax.legend(legend_elements, ['Before release', 'Release', 'After release', 'Release date'])
+
+    # Add Grid
+    ax.grid(True, linestyle='--', alpha=0.5)
+
+    # Add background color
+    ax.set_facecolor('#f4f4f4')
+
+    # Add horizontal grid lines
+    ax.axhline(0, color='black',linewidth=0.5)
+
+    # Add a subtle border to the bars
+    for rect in ax.patches:
+        rect.set_linewidth(0.2)
+        rect.set_edgecolor('black')
+
+def plot_line(data_dict: dict, release: str, title: str):
+    """
+    This function plots the data given by data_dict as an interactive line plot using Plotly
+    :param data_dict: a dictionary where keys are datetimes and values are floats
+    :param release_date is a special date that is highlighted with a vertical dotted line in the plot
+    :param title: is the title of the plot
+    Keep in mind that this function creates a very beautiful and appealing plot with beautiful colours
+    and follows all best practices of Python line plotting
+    """
+    # Extracting dates and values from data_dict
+    dates = list(data_dict.keys())
+    values = list(data_dict.values())
+
+    # Convert release_date to timestamp format
+    release = datetime.strptime(release, '%d-%m-%Y')
+
+    # Selecting a fraction of dates to display on the x-axis
+    tick_fraction = 20
+    subset_dates = dates[::tick_fraction]
+
+    # Creating a trace for the data line
+    trace = go.Scatter(x=dates, y=values, mode='lines+markers', name='Data Line', marker=dict(color='skyblue', size=1.5))
+
+    # Creating a trace for the release date line
+    release_trace = go.Scatter(x=[release, release], y=[min(values), max(values)],
+                               mode='lines', name='Release Date', line=dict(color='red', dash='dash'))
+
+    # Creating the layout
+    layout = go.Layout(title=title, xaxis=dict(title='Date', tickangle=45, tickmode='array', tickvals=subset_dates,
+                                            #    ticktext=[date.strftime('%Y-%m-%d') for date in subset_dates],
+                                               ticktext=[date for date in subset_dates],
+                                               showgrid=False),
+                       yaxis=dict(title='Values'), showlegend=True, hovermode='closest',
+                       plot_bgcolor='rgba(240, 240, 240, 0.6)')
+    
+    # add vertical lines for release date
+    layout.update(dict(shapes=[dict(type='line', xref='x', yref='paper', x0=release, y0=0, x1=release, y1=1,
+                                    line=dict(color='red', width=2, dash='dash'))]))
+
+    # Combining traces and layout into a figure
+    fig = go.Figure(data=[trace, release_trace], layout=layout)
+
+    return fig
